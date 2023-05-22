5b5a8c4537a3d56a57fec80f66eb78e6a98c32b0
Arthur Gabriel <123274385+arthurgmv@users.noreply.github.com>
2023-05-22 18:45:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 window.addEventListener('load', function() {
-    var socialLinks = document.querySelectorAll('.social-links a');
-  
-    for (var i = 0; i < socialLinks.length; i++) {
-      socialLinks[i].addEventListener('click', function(event) {
-        event.preventDefault();
-        var url = this.getAttribute('href');
-        window.open(url, '_blank');
-      });
-    }
-  });
- 
+  var socialLinks = document.querySelectorAll('.social-links a');
+
+  for (var i = 0; i < socialLinks.length; i++) {
+    socialLinks[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      var url = this.getAttribute('href');
+      window.open(url, '_blank', 'noopener');
+    });
+  }
+});
