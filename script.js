899b3dc948eb7721e5ce9be749df679193527e56
Arthur Gabriel <123274385+arthurgmv@users.noreply.github.com>
2023-05-22 18:13:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function() {
+    var socialLinks = document.querySelectorAll('.social-links a');
+  
+    for (var i = 0; i < socialLinks.length; i++) {
+      socialLinks[i].addEventListener('click', function(event) {
+        event.preventDefault();
+        var url = this.getAttribute('href');
+        window.open(url, '_blank');
+      });
+    }
+  });
+ 
